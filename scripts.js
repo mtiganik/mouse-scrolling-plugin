4cb1021ae9db1a7d365025a76879f9ba73cee669
mtiganik <mtiganik@gmail.com>
2024-12-27 17:20:50 +0200

    work with mouse movement

--- scripts.js
+++ scripts.js
@@ -1,1 +1,32 @@
 console.log("Hello world")
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    fetch("mouse.svg")
+    .then(response => response.text())
+    .then(svgContent => {
+        document.getElementById("svgContainer").innerHTML = svgContent;
+        setupAnimation();
+    })
+})
+
+function setupAnimation(){
+    const mouse = document.querySelector("#svgContainer svg")
+    // const animate = decodeURIComponent
+    const animateMouseButton = document.getElementById("animateMouse");
+
+    animateMouseButton.addEventListener("click", () => {
+        mouse.classList.add("animate");
+        setTimeout(() => {
+            mouse.classList.remove("animate");
+        },600);
+    });
+
+    document.addEventListener("keydown", (event) => {
+        if(event.code === "Space"){
+            mouse.classList.add("animate");
+            setTimeout(() => {
+                mouse.classList.remove("animate");
+            }, 600);
+        }
+    })
+}
