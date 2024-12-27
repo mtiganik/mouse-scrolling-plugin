85f1a3b4d2163f4bdc8d43b7fd0c043fd02af396
mtiganik <mtiganik@gmail.com>
2024-12-27 19:26:18 +0200

    mouse moves right to left

--- scripts.js
+++ scripts.js
@@ -5,9 +5,32 @@ document.addEventListener("DOMContentLoaded", ()=> {
     .then(response => response.text())
     .then(svgContent => {
         document.getElementById("svgContainer").innerHTML = svgContent;
-        setupAnimation();
+        setupScrollTracking();
+        // setupAnimation()
     })
+    .catch(err => console.error("Failed to load Mouse SVG"))
 })
+
+function setupScrollTracking(){
+    const svgContainer = document.getElementById("svgContainer");
+
+    // Track scrolling
+    window.addEventListener("scroll",() => {
+        const scrollTop = window.scrollY; // How much user has scrolled
+
+        const docHeight = document.body.scrollHeight; // Total document height
+        const winHeight = window.innerHeight; // Height of the viewport
+        const scrollPercent = scrollTop / (docHeight - winHeight)
+        console.log("Percent: ", scrollPercent)
+
+        const maxMove = window.innerWidth;
+        const newX = maxMove *(scrollPercent);
+        console.log("newx:",newX)
+
+        svgContainer.style.transform =`translateX(-${newX}px)`
+        // svgContainer.style.transform =`translateX(-10px)`
+    });
+}
 
 function setupAnimation(){
     const mouse = document.querySelector("#svgContainer svg")
