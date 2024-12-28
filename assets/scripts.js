37edb28c3189299342a053470646195d42060645
mtiganik <mtiganik@gmail.com>
2024-12-28 18:38:09 +0200

    start to make an plugin out of it

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,14 +6,13 @@ document.addEventListener("DOMContentLoaded", ()=> {
     .then(svgContent => {
         document.getElementById("svgContainer").innerHTML = svgContent;
         setupScrollTracking();
-        // setupAnimation()
     })
     .catch(err => console.error("Failed to load Mouse SVG"))
 })
 
 function setupScrollTracking(){
     const svgContainer = document.getElementById("svgContainer");
-
+    let lastScrollTop = 0
     // Track scrolling
     window.addEventListener("scroll",() => {
         const scrollTop = window.scrollY; // How much user has scrolled
@@ -21,14 +20,24 @@ function setupScrollTracking(){
         const docHeight = document.body.scrollHeight; // Total document height
         const winHeight = window.innerHeight; // Height of the viewport
         const scrollPercent = scrollTop / (docHeight - winHeight)
-        console.log("Percent: ", scrollPercent)
 
         const maxMove = window.innerWidth;
         const newX = maxMove *(scrollPercent);
-        console.log("newx:",newX)
 
-        svgContainer.style.transform =`translateX(-${newX}px)`
-        // svgContainer.style.transform =`translateX(-10px)`
+        const scrollDirection = scrollTop > lastScrollTop ? "down" : "up"
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+
+        if (scrollDirection === "down"){
+
+            svgContainer.style.transform =`translateX(-${newX}px) scaleX(1)`
+        }else{
+            svgContainer.style.transform =`translateX(-${newX}px) scaleX(-1)`
+        }
+
+        svgContainer.classList.add("animate")
+        setTimeout(() => {
+            svgContainer.classList.remove("animate")
+        },600)
     });
 }
 
---
+++ test/scripts.js
@@ -0,0 +1,64 @@
+console.log("Hello world")
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    fetch("mouse.svg")
+    .then(response => response.text())
+    .then(svgContent => {
+        document.getElementById("svgContainer").innerHTML = svgContent;
+        setupScrollTracking();
+    })
+    .catch(err => console.error("Failed to load Mouse SVG"))
+})
+
+function setupScrollTracking(){
+    const svgContainer = document.getElementById("svgContainer");
+    let lastScrollTop = 0
+    // Track scrolling
+    window.addEventListener("scroll",() => {
+        const scrollTop = window.scrollY; // How much user has scrolled
+
+        const docHeight = document.body.scrollHeight; // Total document height
+        const winHeight = window.innerHeight; // Height of the viewport
+        const scrollPercent = scrollTop / (docHeight - winHeight)
+
+        const maxMove = window.innerWidth;
+        const newX = maxMove *(scrollPercent);
+
+        const scrollDirection = scrollTop > lastScrollTop ? "down" : "up"
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+
+        if (scrollDirection === "down"){
+
+            svgContainer.style.transform =`translateX(-${newX}px) scaleX(1)`
+        }else{
+            svgContainer.style.transform =`translateX(-${newX}px) scaleX(-1)`
+        }
+
+        svgContainer.classList.add("animate")
+        setTimeout(() => {
+            svgContainer.classList.remove("animate")
+        },600)
+    });
+}
+
+function setupAnimation(){
+    const mouse = document.querySelector("#svgContainer svg")
+    // const animate = decodeURIComponent
+    const animateMouseButton = document.getElementById("animateMouse");
+
+    animateMouseButton.addEventListener("click", () => {
+        mouse.classList.add("animate");
+        setTimeout(() => {
+            mouse.classList.remove("animate");
+        },600);
+    });
+
+    document.addEventListener("keydown", (event) => {
+        if(event.code === "Space"){
+            mouse.classList.add("animate");
+            setTimeout(() => {
+                mouse.classList.remove("animate");
+            }, 600);
+        }
+    })
+}
